f4a698bfd6c08adac9c0754961a02b4129abec5f
hofiorg <hofiorg@googlemail.com>
2016-05-14 12:06:06 +0200

    ftp upload

--- generator/src/FTPUpload.java
+++ generator/src/FTPUpload.java
@@ -1,26 +1,66 @@
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class FTPUpload {
-  FTPClient client = new FTPClient();
-  FileInputStream fis = null;
+
+  private FTPClient client = new FTPClient();
+  private FileInputStream fis = null;
 
 
   public static void main(String[] args) {
     new FTPUpload().upload();
   }
 
+  private Properties loadProperties() throws IOException {
+    Properties p = new Properties();
+    p.load(getClass().getClassLoader().getResourceAsStream("ftp.properties"));
+    return p;
+  }
+
   private void upload() {
     try {
-      client.connect("ftp.hofi.org");
-      client.login("yyyyyyy", "xxxxxxxx");
+      Properties prop = loadProperties();
 
-      String filename = "html/test.txt";
-      fis = new FileInputStream(filename);
+      String server = (String)prop.get("server");
+      client.connect(server);
 
-      client.storeFile(filename, fis);
+      System.out.println("Connected to " + server + ".");
+      System.out.print(client.getReplyString());
+
+      client.login((String)prop.get("login"), (String)prop.get("password"));
+
+      removeDirectoryRecursive(client, "/html/article");
+
+      System.out.println("");
+
+
+      Files.walk(Paths.get("web" + File.separator + "article")).forEach(filePath -> {
+        try {
+          System.out.println("store: "+ filePath);
+          String newName = filePath.toString().replace("web", "html");
+          if (Files.isRegularFile(filePath)) {
+            // file
+            fis = new FileInputStream(filePath.toString());
+            client.storeFile(newName, fis);
+
+          } else {
+            // directory
+            client.makeDirectory(newName);
+          }
+        }
+        catch(IOException ioe) {
+          ioe.printStackTrace();
+        }
+
+      });
+
       client.logout();
     } catch (IOException e) {
       e.printStackTrace();
@@ -34,5 +74,19 @@ public class FTPUpload {
         e.printStackTrace();
       }
     }
+  }
+
+  private void removeDirectoryRecursive(FTPClient client, String dir) throws IOException{
+    System.out.println("remove: " + dir);
+    FTPFile[] list = client.listFiles(dir);
+    for(FTPFile f : list) {
+      if(f.getType() == 0) {
+        client.deleteFile(dir + "/" + f.getName());
+      }
+      if(f.getType() == 1) {
+        removeDirectoryRecursive(client, dir + "/" + f.getName());
+      }
+    }
+    client.removeDirectory(dir);
   }
 }
