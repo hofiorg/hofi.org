2547b9374f7d257947fe1a16be6ee2057fa65de3
hofiorg <hofiorg@googlemail.com>
2016-05-21 11:41:12 +0200

    dist

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -2,17 +2,19 @@
 
 cd ..
 
-rm -R bower_components
+# remove old installation
+rm -rf bower_components
+rm -rf web/bower_components
+rm -rf web/vulcanized
 
+# bower
 bower install
 
-rm -R web/bower_components
-rm -R web/vulcanized
-
-mkdir web/vulcanized
-
-vulcanize elements/elements.html -o web/vulcanized/elements.html --inline-scripts --inline-css --strip-comments
-vulcanize elements/elements_article.html -o web/vulcanized/elements_article.html --inline-scripts --inline-css --strip-comments
-
+# copy jquery to web
 mkdir web/bower_components
 cp -R bower_components/jquery web/bower_components/jquery
+
+# vulcanize for production
+mkdir web/vulcanized
+vulcanize elements/elements.html -o web/vulcanized/elements.html --inline-scripts --inline-css --strip-comments
+vulcanize elements/elements_article.html -o web/vulcanized/elements_article.html --inline-scripts --inline-css --strip-comments
